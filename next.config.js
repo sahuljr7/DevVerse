677e917e59f6e446b3871121b0ec23c78e6de5da
sahuljr7 <sahulkumarparida@gmail.com>
2024-04-08 15:41:19 +0530

    fourth

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   reactStrictMode: true,
   // below resolve module not found on fs module
   webpack: (config, { isServer }) => {
@@ -11,10 +14,6 @@ const nextConfig = {
     }
     return config
   }
-  // above resolve module not found on fs module
-  // if you want to use external image, for example from unsplash, you need to set the images property and add to it the url of the image, uncomment the following line.
-  // more info: on https://nextjs.org/docs/basic-features/image-optimization#domains
-  // images: ['images.unsplash.com']
 }
 
 module.exports = nextConfig
